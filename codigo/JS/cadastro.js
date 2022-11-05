833f68ef608723a6a0dd7027d06c711f26206142
Fulano de Tal <alexandrejrjuninho@gmail.com>
2022-11-05 15:22:12 -0300

    Alteracao inicial no script

--- codigo/JS/cadastro.js
+++ codigo/JS/cadastro.js
@@ -3,23 +3,27 @@ function Cadastrar(){
     var email = document.getElementById('email').value;
     var senha = document.getElementById('senha').value;
     var submit = document.getElementsByName('submit');
-    const usuario = {'usuario':nome, 'email':email, 'senha':senha};
-    localStorage.setItem('info', JSON.stringify(usuario));
+    
 
 
     if(senha.length  < 8 && (nome == "" || email == "" || senha == "")){
         document.getElementById('alerta').className = "ativo";
-        document.getElementById('alerta').innerText = "Você deixou algo em branco!"
-
+        document.getElementById('alerta').innerText = "Sua senha deve ter pelo menos 8 caracteres! E voce deixou algo em branco!"
+        
     }else if(nome == "" || email == "" || senha == ""){
         document.getElementById('alerta').className = "ativo";
-        document.getElementById('alerta').innerText = "Sua senha deve ter pelo menos 8 caracteres! E voce deixou algo em branco!"
+        document.getElementById('alerta').innerText = "Você deixou algo em branco!"
+        
     }else if(senha.length < 8){
         document.getElementById('alerta').className = "ativo";
         document.getElementById('alerta').innerText = "Sua senha deve ter pelo menos 8 caracteres!"
+    }else{
+        const usuario = {'usuario':nome, 'email':email, 'senha':senha};
+        localStorage.setItem('info', JSON.stringify(usuario));
+        window.location.href = "login.html"
     }
 
-    window.location.href = "login.html"
+    
 
     
 
